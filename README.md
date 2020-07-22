40c4590872030ea0d5045018aba66570ba5c29b1
M. J. Steil <msteil@theorie.ikp.physik.tu-darmstadt.de>
2020-07-22 18:13:53 +0200

    Reworked output and Example Problems
    - Added the field 'name' -- problem name -- to all problems (user and example) ones
    - Added Sod and Lax shock tube problems
    - Filename for output is now based on problem name and time
    - Added momentum density $\mu$ and energy density $\epsilon$ as output columns

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ ToroExact is a Python program meant to generate exact solutions to Riemann probl
 ## Installation
 
 ```
-git clone https://github.com/tahandy/ToroExact.git
+git clone https://github.com/MJSteil/ToroExact.git
 ````
 
 ## Source structure
@@ -18,13 +18,14 @@ git clone https://github.com/tahandy/ToroExact.git
 ## Usage
 
 ```
-usage: toro_exact.py [-h] -p {1,2,3,4,5,6,7,all,user} [-g GAMMA]
-                     [-d {zonal,nodal}] [-n NPTS] [-b BOUNDS] [-l STATEL]
-                     [-r STATER] [-x X0] [-t TIME]
+usage: toro_exact.py [-h] -p
+                     {Sod,Lax,Toro-1,Toro-2,Toro-3,Toro-4,Toro-5,Toro-6,Toro-7,all,user}
+                     [-g GAMMA] [-d {zonal,nodal}] [-n NPTS] [-b BOUNDS]
+                     [-l STATEL] [-r STATER] [-x X0] [-t TIME] [-name NAME]
 
 optional arguments:
   -h, --help            show this help message and exit
-  -p {1,2,3,4,5,6,7,all,user}, --problem {1,2,3,4,5,6,7,all,user}
+  -p {Sod,Lax,Toro-1,Toro-2,Toro-3,Toro-4,Toro-5,Toro-6,Toro-7,all,user}, --problem {Sod,Lax,Toro-1,Toro-2,Toro-3,Toro-4,Toro-5,Toro-6,Toro-7,all,user}
                         select which problem to solve
   -g GAMMA, --gamma GAMMA
                         set ideal gas gamma
@@ -40,6 +41,8 @@ optional arguments:
                         set user right state "[dens, vel, pres]"
   -x X0, --x0 X0        set user diapgram location
   -t TIME, --time TIME  set user evaluation time
+  -name NAME, --name NAME
+                        problem name
 ```
 * **-p,--problem [REQUIRED]** defines which problem(s) to evaluate. Can be used multiple times (e.g. ```-p 1 -p 4 -p user```).
 
@@ -61,6 +64,8 @@ optional arguments:
 
 * **-t,--time** defines the time at which the exact solution is evaluated.
 
+* **-name,--name** defines the name of the user problem.
+
 ## Examples
 
 ### Generate all Toro problems on a 100 zone grid
@@ -69,20 +74,23 @@ optional arguments:
 
 ### Generate solutions to only problems 1 and 7 using default values
 
-```./toro_exact.py -p 1 -p 7```
+```./toro_exact.py -p Toro-1 -p Toro-7```
 
 ### Generate a user-defined problem on the domain [0.0,1.0] with a nodal grid of 25 points
 
-```./toro_exact.py -n 25 -d nodal -p user --left "[1.0,0.0,1.0]" --right "[0.125,0.0,0.1]" -b "[0.0,1.0]" -x 0.5 -t 0.2```
+```./toro_exact.py -n 25 -d nodal -p user --left "[1.0,0.0,1.0]" --right "[0.125,0.0,0.1]" -b "[0.0,1.0]" -x 0.5 -t 0.2 -name "'custom'"```
 
 ## Reference
 
-[1]: E.F. Toro. *Riemann solvers and numerical methods for fluid dynamics: a practical introduction*. Springer, Berlin, New York, 2009.
+[1]: E.F. Toro. *Riemann solvers and numerical methods for fluid dynamics: a practical introduction*. Springer, Berlin, New York, 2009.  
+[2]: G. A. Sod, J. Comput. Phys. 27.1 (1978)  
+[3]: P. D. Lax, Comm. Pure Appl. Math. 7.1 (1954)
 
 ## License
 MIT License
 
-Copyright (c) 2017 Timothy Handy
+Original work Copyright 2017 Timothy A. Handy  
+Modified work Copyright 2020 Martin J. Steil
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- toro_exact.py
+++ toro_exact.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-## Copyright 2017 Timothy A. Handy
+## Original work Copyright 2017 Timothy A. Handy
+## Modified work Copyright 2020 Martin J. Steil
 ##
 ## Permission is hereby granted, free of charge, to any person obtaining
 ## a copy of this software and associated documentation files (the "Software"),
@@ -26,16 +27,21 @@ import exactRP
 import argparse
 
 #============================================
-# Problem definitions for Toro (1999) problems
+# Selected Euler Shock Tube Problems from 
+# [G. A. Sod, J. Comput. Phys. 27.1 (1978)]
+# [P. D. Lax, Comm. Pure Appl. Math. 7.1 (1954)]
+# [E. Toro, Riemann Solvers and Numerical Methods for Fluid Dynamics, Berlin: Springer, 1999]
 #============================================
 cases = {
-	'1': {'left':[1.0,      0.75,       1.0],   'right':[0.125,     0.0,      0.1],    'time':0.2,   'x0':0.3},
-	'2': {'left':[1.0,     -2.0,        0.4],   'right':[1.0,       2.0,      0.4],    'time':0.15,  'x0':0.5},
-	'3': {'left':[1.0,      0.0,     1000.0],   'right':[1.0,       0.0,      0.01],   'time':0.012, 'x0':0.5},
-	'4': {'left':[5.99924, 19.5975,   460.894], 'right':[5.99242,  -6.19633, 46.0950], 'time':0.035, 'x0':0.4},
-	'5': {'left':[1.0,    -19.59745, 1000.0],   'right':[1.0,     -19.59745,  0.01],   'time':0.012, 'x0':0.8},
-	'6': {'left':[1.4,      0.0,        1.0],   'right':[1.0,       0.0,      1.0],    'time':2.0,   'x0':0.5},
-	'7': {'left':[1.4,      0.1,        1.0],   'right':[1.0,       0.1,      1.0],    'time':2.0,   'x0':0.5},
+	'Sod':    {'left':[1.0,      0.0,       1.0],    'right':[0.125,     0.0,      0.1],    'time':0.1644,'x0':0.5,  'name':'Sod'},
+	'Lax':    {'left':[0.445,0.698876,   3.52773],   'right':[0.5,     0.0,      0.571],    'time':0.16,  'x0':0.5,  'name':'Lax'},
+	'Toro-1': {'left':[1.0,      0.75,       1.0],   'right':[0.125,     0.0,      0.1],    'time':0.2,   'x0':0.3,  'name':'Toro-1'},
+	'Toro-2': {'left':[1.0,     -2.0,        0.4],   'right':[1.0,       2.0,      0.4],    'time':0.15,  'x0':0.5,  'name':'Toro-2'},
+	'Toro-3': {'left':[1.0,      0.0,     1000.0],   'right':[1.0,       0.0,      0.01],   'time':0.012, 'x0':0.5,  'name':'Toro-3'},
+	'Toro-4': {'left':[5.99924, 19.5975,   460.894], 'right':[5.99242,  -6.19633, 46.0950], 'time':0.035, 'x0':0.4,  'name':'Toro-4'},
+	'Toro-5': {'left':[1.0,    -19.59745, 1000.0],   'right':[1.0,     -19.59745,  0.01],   'time':0.012, 'x0':0.8,  'name':'Toro-5'},
+	'Toro-6': {'left':[1.4,      0.0,        1.0],   'right':[1.0,       0.0,      1.0],    'time':2.0,   'x0':0.5,  'name':'Toro-6'},
+	'Toro-7': {'left':[1.4,      0.1,        1.0],   'right':[1.0,       0.1,      1.0],    'time':2.0,   'x0':0.5,  'name':'Toro-7'}
 }
 
 #============================================
@@ -47,7 +53,7 @@ def setupParser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-p','--problem',dest='problems',
 		                action='append', required=True,
-		                choices=['1','2','3','4','5','6','7','all','user'],
+		                choices=[*cases]+['all','user'],
 		                help='select which problem to solve')
 	parser.add_argument('-g','--gamma',dest='gamma',
 		                default=1.4,
@@ -71,6 +77,8 @@ def setupParser():
 		                    help='set user diapgram location')
 	user_group.add_argument('-t','--time',dest='time',
 		                    help='set user evaluation time')
+	user_group.add_argument('-name','--name',dest='name',
+		                    help='set problem name')
 	return parser
 
 #============================================
@@ -87,7 +95,7 @@ if(__name__ == '__main__'):
 	# Extract parameters relevant to all problems
 	problemList = args.problems
 	if('all' in problemList):
-		tmp = ['1','2','3','4','5','6','7']
+		tmp = [*cases]
 		if('user' in problemList):
 			tmp.append('user')
 		problemList = tmp
@@ -100,13 +108,14 @@ if(__name__ == '__main__'):
 	# Extract user parameters
 	if('user' in problemList):
 		if(args.stateL is None or args.stateR is None
-		   or args.x0 is None or args.time is None):
-			parser.error('User problems require state{L/R}, x0 and time')
+		   or args.x0 is None or args.time is None or args.name is None):
+			parser.error('User problems require state{L/R}, x0, time and name')
 
 		cases['user'] = {'left':ast.literal_eval(args.stateL),
 		                 'right':ast.literal_eval(args.stateR),
 		                 'time':ast.literal_eval(args.time),
-		                 'x0':ast.literal_eval(args.x0)}
+		                 'x0':ast.literal_eval(args.x0),
+		                 'name':ast.literal_eval(args.name)}
 
 	# Evaluate all requested problems
 	for icase in problemList:
@@ -114,6 +123,7 @@ if(__name__ == '__main__'):
 		stateR = cases[icase]['right']
 		t      = cases[icase]['time']
 		x0     = cases[icase]['x0']
+		name   = cases[icase]['name']
 
 		sys.stdout.write('Running problem: {:s}\n'.format(icase))
 		sys.stdout.write('  xbnds: {:f} {:f}\n'.format(xbnds[0],xbnds[1]))
@@ -123,6 +133,7 @@ if(__name__ == '__main__'):
 		sys.stdout.write('  right: {:f}  {:f}  {:f}\n'.format(stateR[0], stateR[1], stateR[2]))
 		sys.stdout.write('  gamma: {:f}\n'.format(gamma))
 		sys.stdout.write('  time:  {:f}\n'.format(t))
+		sys.stdout.write('  name:  {:s}\n'.format(name))
 
 		# Solve the exact RP, which produces
 		# the state in the star region
@@ -152,24 +163,18 @@ if(__name__ == '__main__'):
 		dens, pres, velx, eint, cspd = rp.sample(s)
 
 		# Write to output
-		filename = 'output/toro_{:s}_exact.dat'.format(icase)
+		filename = 'output/{:s}_t{:.5e}_exact.csv'.format(name,t)
 		sys.stdout.write('  Writing to file: {:s}\n'.format(filename))
 		with open(filename,'w+') as file:
-			file.write('# Exact Riemann solution for problem: {:s}\n'.format(icase))
-			file.write('# Discretization: {:s}\n'.format(disc))
-			file.write('# Domain bounds: [{:<.5e},{:.5e}]\n'.format(xbnds[0],xbnds[1]))
-			file.write('# Iterface position: {:.5e}\n'.format(x0))
-			file.write('# Gamma: {:.5e}\n'.format(gamma))
-			file.write('# Left  state [dens,velx,pres]: [{:<.5e}, {:.5e}, {:.5e}]\n'.format(stateL[0],stateL[1],stateL[2]))
-			file.write('# Right state [dens,velx,pres]: [{:<.5e}, {:.5e}, {:.5e}]\n'.format(stateR[0],stateR[1],stateR[2]))
-			file.write('# Time: {:.5e}\n'.format(t))
-			file.write('{:>17} {:>17} {:>17} {:>17} {:>17} {:>17}\n'.format('x','dens','pres','velx','eint','cspd'))
+			file.write('# Exact Riemann solution for problem:\t{:s}\n'.format(name))
+			file.write('# Discretization:\t{:s}\n'.format(disc))
+			file.write('# Domain bounds:\t{:<.5e}\t{:.5e}\n'.format(xbnds[0],xbnds[1]))
+			file.write('# Iterface position:\t{:.5e}\n'.format(x0))
+			file.write('# Gamma:\t{:.5e}\n'.format(gamma))
+			file.write('# Left  state [dens,velx,pres]:\t{:<.5e}\t{:.5e}\t{:.5e}\n'.format(stateL[0],stateL[1],stateL[2]))
+			file.write('# Right state [dens,velx,pres]:\t{:<.5e}\t{:.5e}\t{:.5e}\n'.format(stateR[0],stateR[1],stateR[2]))
+			file.write('# Time:\t{:.5e}\n'.format(t))
+			file.write('{:>17}\t{:>17}\t{:>17}\t{:>17}\t{:>17}\t{:>17}\t{:>17}\t{:>17}\n'.format('x','dens [$\\rho$]','mom [$\\mu$]','e [$\\epsilon$]','pres [$p$]','eint','velx [$v$]','cspd'))
 			for i in range(0,npts):
-				file.write('{:17.9e} {:17.9e} {:17.9e} {:17.9e} {:17.9e} {:17.9e}\n'.format(x[i],dens[i],pres[i],velx[i],eint[i],cspd[i]))
-
-
-
-
-
-
+				file.write('{:17.9e}\t{:17.9e}\t{:17.9e}\t{:17.9e}\t{:17.9e}\t{:17.9e}\t{:17.9e}\t{:17.9e}\n'.format(x[i],dens[i],dens[i]*velx[i],pres[i]/(gamma-1.0)+0.5*velx[i]*velx[i]*dens[i],pres[i],eint[i],velx[i],cspd[i]))
 
